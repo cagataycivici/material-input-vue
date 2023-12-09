f63f1641da3f0fee16a5049a642c5ae990a6393c
Cagatay Civici <cagatay.civici@gmail.com>
2023-12-09 15:17:39 +0300

    Add filled and outlined

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,19 @@
             <ColorPalette />
         </div>
 
-        <span class="relative">
-            <InputText id="username" autocomplete="off" class="peer" placeholder="" />
-            <label for="username"
-                class="absolute text-sm text-primary-500 dark:text-primary-400/60 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Username</label>
-        </span>
+        <div class="flex gap-4">
+            <span class="relative">
+                <InputText id="username1" autocomplete="off" class="peer" placeholder="" />
+                <label for="username1"
+                    class="bg-white dark:bg-surface-900 px-2 absolute text-sm text-primary-500 dark:text-primary-400/60 duration-300 transform -translate-y-6 text-primary-500 dark:text-primary-400 -scale-75 top-4 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-surface-500 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-primary-500">Username</label>
+            </span>
+
+            <span class="relative">
+                <InputText id="username2" autocomplete="off" class="peer" placeholder="" mode="filled" />
+                <label for="username2"
+                    class="absolute text-sm text-primary-500 dark:text-primary-400/60 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Username</label>
+            </span>
+        </div>
     </div>
 </template>
 
--- src/mypreset/inputtext/index.js
+++ src/mypreset/inputtext/index.js
@@ -1,28 +1,32 @@
 export default {
-    root: ({ context }) => ({
+    root: ({ instance, state }) => ({
         class: [
             // Font
-            "font-sans leading-none px-2.5 pb-2.5 pt-5",
+            "font-sans leading-none",
+            { " px-3 pb-3 pt-5": instance.$attrs.mode === "filled" },
+            { " px-3 pb-4 pt-4": instance.$attrs.mode !== "filled" },
 
             // Colors
-            "text-primary-600 dark:text-primary-300",
-            "placeholder:text-primary-400 dark:placeholder:text-primary-400/60",
-            "bg-primary-50 dark:bg-primary-400/10",
-            "border-b-2 border-primary-400 dark:border-primary-400/60",
+            {
+                "text-primary-600 dark:text-primary-300 bg-primary-50 dark:bg-primary-400/10 border-b-2 border-primary-400 dark:border-primary-400/60 rounded-none":
+                    instance.$attrs.mode === "filled",
+            },
+            {
+                "text-surface-700 dark:text-surface-300 bg-white dark:bg-surface-900 border border-surface-300 dark:border-surface-700 rounded-lg":
+                    instance.$attrs.mode !== "filled",
+            },
 
             // States
             {
                 "hover:border-primary-500 dark:hover:border-primary-400":
-                    !context.disabled,
+                    !state.disabled,
                 "focus:outline-none focus:border-primary-600 dark:focus:border-primary-300":
-                    !context.disabled,
+                    !state.disabled,
                 "opacity-60 select-none pointer-events-none cursor-default":
-                    context.disabled,
+                    state.disabled,
             },
 
             // Misc
-            "rounded-none",
-            "appearance-none",
             "transition-colors duration-200",
         ],
     }),
